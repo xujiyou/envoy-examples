f558bfd6ea136c054861d5c7042e9fdc8f10fbd0
xujiyou <xujiyou555@gmail.com>
2020-04-26 12:54:19 +0800

    jwt

---
+++ jwt/go.mod
@@ -0,0 +1,8 @@
+module jwt
+
+go 1.14
+
+require (
+	github.com/gbrlsnchs/jwt/v3 v3.0.0-rc.2
+	github.com/gin-gonic/gin v1.6.2
+)
---
+++ jwt/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = r.Run("0.0.0.0:9090")
+}
